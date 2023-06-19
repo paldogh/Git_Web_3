d418d4fe5cbeccb182abadd8d50ca3084ff83a04
paldogh <aldo.h.p@hotmail.com>
2023-06-19 07:15:22 -0600

    GALERY

---
+++ PracticasWeb_Aldo23/p4_sitioWeb/js/menu.js
@@ -0,0 +1,8 @@
+let btnMenu= document.getElementById('btnmenu');
+let menu= document.getElementById('menu');
+
+btnMenu.addEventListener('click', function(){
+'user strict';
+menu.classList.toggle('mostrar');
+
+})
---
+++ PracticasWeb_Aldo23/p4_sitioWeb/js/modal.js
@@ -0,0 +1,18 @@
+let imagenes=document.querySelectorAll('.galeria__img');
+let modal=document.querySelector('#modal');
+let imagen=document.querySelector('#modal_img');
+let boton=document.querySelector('#modal_boton');
+for (let i = 0; i < imagenes.length; i++) {
+  imagenes[i].addEventListener('click',function(e){
+modal.classList.toggle("modal--open");
+    let src=e.target.src;
+imagen.setAttribute("src",src);
+
+
+})
+}
+boton.addEventListener('click',function() {
+
+  modal.classList.toggle("modal--open");
+
+} )
